ff8b3e2aa95b4067833b46338146d481e768a407
Victor Cebanu <victor.cebanu@ellation.com>
2021-06-21 09:40:40 +0300

    Initial Commit

---
+++ renovate_test_public/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  renovate_test_public
+//
+//  Created by Victor Cebanu on 21.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
